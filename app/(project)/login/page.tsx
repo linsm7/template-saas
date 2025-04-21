743cf0610f956c6c4c411956f8fe578692742e27
Matheus <lins.dv7@gmail.com>
2025-04-21 17:30:12 -0300

    add protected login route

--- app/(project)/login/page.tsx
+++ app/(project)/login/page.tsx
@@ -1,7 +1,15 @@
 import { handleAuth } from "@/app/actions/handle-auth"
 import { Button } from "@/app/components/ui/button"
+import { auth } from "@/app/lib/auth"
+import { redirect } from "next/navigation"
 
-export default function Login () {
+export default async function Login () {
+    const session = await auth()
+    
+    if(session) {
+        redirect("/dashboard")
+    }
+
     return (
         <div className="h-screen w-full flex flex-col items-center justify-center gap-4">
             <h1 className="font-semibold">Login</h1>
